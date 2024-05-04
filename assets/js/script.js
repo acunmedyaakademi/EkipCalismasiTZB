8c8f265fbe22f10098531faa36cd4c7a82c394cb
Zehra Ecer <zehraecer1996@hotmail.com>
2024-05-04 14:21:45 +0300

    header yapıldı

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// zehra
+
+
+function qs(selector) {
+    let element = document.querySelector(selector)
+
+    return element
+}
+
+const sunmode = qs(".sunmode")
+const darkmode = qs(".darkmode")
+const body = qs("body")
+
+sunmode.addEventListener("click", (e) => {
+    console.log("sdjngdkfjs");
+    body.classList.add("darkModeActive")
+    sunmode.style.display = "none"
+    darkmode.style.display = "flex"
+})
+
+darkmode.addEventListener("click", (e) => {
+    body.classList.remove("darkModeActive")
+    sunmode.style.display = "flex"
+    darkmode.style.display = "none"
+})
+
+// zehra
